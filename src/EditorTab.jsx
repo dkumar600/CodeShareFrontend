de9822001d5f954da7fbf7280e9b58e015287f03
dkumar600 <dktiwary007@gmail.com>
2023-12-03 22:33:30 +0530

    fix for "type something here"

--- src/EditorTab.jsx
+++ src/EditorTab.jsx
@@ -45,7 +45,14 @@ export default function EditorTab() {
       });
     }
   },[codeSave,Lang,editorCode]);
-
+  function textCheck(editor){
+    const mText = document.querySelector(".monaco-text");
+    if (editor.getValue()) {
+      mText.style.display = "none";
+    } else {
+      mText.style.display = "flex";
+    }
+  }
   function preTextRemove(value) {
     const mText = document.querySelector(".monaco-text");
     if (value) {
@@ -55,6 +62,7 @@ export default function EditorTab() {
     }
     setEditorCode(()=>value?value:'');
   }
+  
   return (
     <>
       <Editor
@@ -63,6 +71,7 @@ export default function EditorTab() {
         value={codeSave?codeSave.code:''}
         theme="hc-black"
         onChange={preTextRemove}
+        onMount={textCheck}
         
       />
       <div className="monaco-text"></div>
